c6eff1677cd12ccc8233e9b5a34b8e870557c83d
René Klatt <rene.klatt@spryker.com>
2017-01-26 08:38:01 +0000

    core-714 added Quote bundle, removed dependency to Oms in Checkout

--- src/Spryker/Client/Cart/Dependency/Client/CartToQuoteInterface.php
+++ src/Spryker/Client/Cart/Dependency/Client/CartToQuoteInterface.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\Cart\Session;
+namespace Spryker\Client\Cart\Dependency\Client;
 
 use Generated\Shared\Transfer\QuoteTransfer;
 
-interface QuoteSessionInterface
+interface CartToQuoteInterface
 {
 
     /**
@@ -20,21 +20,9 @@ interface QuoteSessionInterface
     /**
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      *
-     * @return $this
+     * @return void
      */
     public function setQuote(QuoteTransfer $quoteTransfer);
-
-    /**
-     * @return int
-     */
-    public function getItemCount();
-
-    /**
-     * @param int $itemCount
-     *
-     * @return $this
-     */
-    public function setItemCount($itemCount);
 
     /**
      * @return void

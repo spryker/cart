0bb4bff897a083d9e14c58d672f9f53430855a4a
Dima Aseev <dmitriy.aseev@spryker.com>
2019-04-09 16:46:46 +0300

    PS-9058: Fixes comments after tech lead review.

--- src/Spryker/Zed/Cart/Business/Locker/QuoteLockerInterface.php
+++ src/Spryker/Zed/Cart/Business/Locker/QuoteLockerInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\Cart\Business\Model;
+namespace Spryker\Zed\Cart\Business\Locker;
 
 use Generated\Shared\Transfer\QuoteResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
